5c4703d54244d347db603a831dee7ca68b32dcf8
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2020-03-26 23:01:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ MQTT_TOPIC_PREFIX=$(jq -r ".mqtt.topic_prefix" $CONFIG_PATH)
 MQTT_USERNAME=$(jq -r ".mqtt.username" $CONFIG_PATH)
 MQTT_PASSWORD=$(jq -r ".mqtt.password" $CONFIG_PATH)
 
-npm install
+npm install -g https://github.com/T-REX-XP/am43-ctrl
 node index.js 02:e5:88:9a:cb:95 02:dc:bc:64:8c:06 --url $MQTT_BROKER_URL
