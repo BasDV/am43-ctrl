6340344be58deaaea1c69b6d78860247ed4b4f2c
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2020-03-26 20:27:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-HVAC_HOST=$(jq -r ".hvac_host" $CONFIG_PATH)
 MQTT_BROKER_URL=$(jq -r ".mqtt.broker_url" $CONFIG_PATH)
 MQTT_TOPIC_PREFIX=$(jq -r ".mqtt.topic_prefix" $CONFIG_PATH)
 MQTT_USERNAME=$(jq -r ".mqtt.username" $CONFIG_PATH)
@@ -17,10 +16,9 @@ if [ "$INSTANCES" -gt 0 ]; then
 	for i in $(seq 0 $(($INSTANCES - 1))); do
 		HVAC_HOST=$(jq -r ".devices[$i].hvac_host" $CONFIG_PATH);
 		MQTT_TOPIC_PREFIX=$(jq -r ".devices[$i].mqtt_topic_prefix" $CONFIG_PATH);
-		if [[ $HVAC_HOST = null ]]; then echo "[ERROR] Missing hvac_host for device $i. Skipping." && continue; fi
 		if [[ $MQTT_TOPIC_PREFIX = null ]]; then echo "[ERROR] Missing mqtt_topic_prefix for device $i. Skipping." && continue; fi
 		echo "Running instance $i for $HVAC_HOST"
-		npx pm2 start index.js --silent -m --merge-logs --name="HVAC_${i}" -- \
+		npx pm2 start index.js --silent -m --merge-logs \
 			--hvac-host="${HVAC_HOST}" \
 			--mqtt-broker-url="${MQTT_BROKER_URL}" \
 			--mqtt-topic-prefix="${MQTT_TOPIC_PREFIX}" \
@@ -31,7 +29,6 @@ if [ "$INSTANCES" -gt 0 ]; then
 else
 	echo "Running in single-instance mode (DEPRECATED)"
 	node index.js \
-		--hvac-host="${HVAC_HOST}" \
 		--mqtt-broker-url="${MQTT_BROKER_URL}" \
 		--mqtt-topic-prefix="${MQTT_TOPIC_PREFIX}" \
 		--mqtt-username="${MQTT_USERNAME}" \
